ef35274e56a3089984d698e4363b8e7f60f4f316
Miller <Miller@MacBook-Pro.local>
2017-10-05 09:34:58 -0700

    added styling to landing page

--- src/js/components/LandingPage/LandingPage.jsx
+++ src/js/components/LandingPage/LandingPage.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 
 export default class LandingPage extends React.Component {
-  constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
   }
-  render () {
+  render() {
     return (
-      <div className='fluid-container'>
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-        
-        <a className="navbar-brand" href="#">Navbar</a>
-          <div className='text-right pull-right'>
-              <a className="nav-link d-inline-block pull-right" href="#">Home <span className="sr-only">(current)</span></a>
-              <a className="nav-link d-inline-block pull-right" href="#">Link</a>
-              <a className="nav-link d-inline-block pull-right disabled" href="#">Disabled</a>
+      <div>
+        <nav className="mainBar clearfix">
+          <a id="logo" href="#">Logo</a>
+          <a id="title" href="#"><strong>NoshSpot</strong></a>
+          <a id="login" href="#">Login</a>
+        </nav>
+        <main>
+          <div id="inputGroup">
+            <input placeholder="Enter Location..." type="text" id="locationSearch" />
+            <span id="go">Go</span>
           </div>
-          <a className="nav-link d-block pull-right" href="#">Home <span className="sr-only">(current)</span></a>
-      </nav>
-    </div>
+        </main>
+      </div>
     )
   }
 }
