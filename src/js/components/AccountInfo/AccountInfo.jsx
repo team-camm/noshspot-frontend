74fd6cedf577fbd1d496f84ce81704e99d1107ee
Alex Black <alexblack426@gmail.com>
2017-10-05 16:57:33 -0700

    fixed styling for account page

--- src/js/components/AccountInfo/AccountInfo.jsx
+++ src/js/components/AccountInfo/AccountInfo.jsx
@@ -10,40 +10,40 @@ export default class Confirmation extends React.Component {
             <div>
                 <NavBar />
                 <div style={{ 'height': '68px' }}></div>
-                <div className='row'>
-                    <div className='verticalMenu col-lg-4'>
-                        <h3 style={{textDecoration: 'underline', 'width': '40%'}}id='accSettings'> Account Settings</h3>
-                        <h3  style={{ 'width': '40%' }}id='deliveryHistory'> Delivery History</h3>
+                <div className='pl-4 row mt-2'>
+                    {/* <div className='col-lg-1'></div> */}
+                    <div className='verticalMenu col-lg-2'>
+                        <p className='h4 pb-2 pt-4 mt-4' id='accSettings'><u>Account Settings</u></p>
+                        <hr />
+                        <p className='h4' id='deliveryHistory'>Delivery History</p>
                     </div>
-                    <div style={{ 'padding': '0', 'float': 'right', 'marginTop': '3%' }} className=" card col-sm-7">
-                        <div className="card-header">
-                            <strong style={{ textAlign: 'center' }}>Account Settings </strong>
-                        </div>
+                    <div className='col-lg-1'></div>
+                    <div style={{'borderColor': 'transparent'}} className="card col-lg-8">
                         <div className="card-block">
                             <p />
                             <h4> Change Personal Information </h4>
                             <p />
                             <div className='container' style={{ 'padding': '1em' }}>
                                 <div style={{ marginBottom: '3%' }} className="form-group row">
-                                    <label for="example-text-input" className="col-2 col-form-label">Email:</label>
+                                    <label htmlFor="example-text-input" className="col-2 col-form-label">Email:</label>
                                     <div className="col-10">
                                         <input className="form-control" type="text" id="" />
                                     </div>
                                 </div>
                                 <div style={{ marginBottom: '3%' }} className="form-group row">
-                                    <label for="example-text-input" className="col-2 col-form-label">Password:</label>
+                                    <label htmlFor="example-text-input" className="col-2 col-form-label">Password:</label>
                                     <div className="col-10">
                                         <input className="form-control" type="text" id="" />
                                     </div>
                                 </div>
                                 <div style={{ marginBottom: '3%' }} className="form-group row">
-                                    <label for="example-text-input" className="col-2 col-form-label">Phone Number:</label>
+                                    <label htmlFor="example-text-input" className="col-2 col-form-label">Phone Number:</label>
                                     <div className="col-10">
                                         <input className="form-control" type="text" id="" />
                                     </div>
                                 </div>
                                 <div style={{ marginBottom: '3%' }} className="form-group row">
-                                    <label for="example-text-input" className="col-2 col-form-label">Adress:</label>
+                                    <label htmlFor="example-text-input" className="col-2 col-form-label">Adress:</label>
                                     <div className="col-10">
                                         <input className="form-control" type="text" id="" />
                                     </div>
@@ -51,6 +51,7 @@ export default class Confirmation extends React.Component {
                             </div>
                         </div>
                     </div>
+                    <div className='col-lg-1'></div>
                 </div>
             </div>
         )
