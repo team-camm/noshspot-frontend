9cf44fad362c8fae7a9c34ef9b418710dc9a76c0
Megala <megalainla@gmail.com>
2017-10-05 15:36:58 -0700

    googleapi

--- src/js/components/LandingPage/LandingPage.jsx
+++ src/js/components/LandingPage/LandingPage.jsx
@@ -5,6 +5,22 @@ export default class LandingPage extends React.Component {
   constructor(props) {
     super(props);
   }
+  geolocate() {
+            if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var geolocation = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
+                var circle = new google.maps.Circle({
+                  center: geolocation,
+                  radius: position.coords.accuracy
+                });
+                autocomplete.setBounds(circle.getBounds());
+              });
+            }
+        }
+        
   render() {
     return (
       <div>
@@ -14,7 +30,7 @@ export default class LandingPage extends React.Component {
         </div>
         <main>
           <div id="inputGroup">
-            <input placeholder="Enter Location..." type="text" id="locationSearch" />
+            <input placeholder="Enter Location..." type="text" id="locationSearch" onFocus={this.geolocate} />
             <span id="go">Go</span>
           </div>
         </main>
