2955f0984143a32986a4dc09b0aff1fd22076002
Alex Black <alexblack426@gmail.com>
2017-10-06 14:21:54 -0700

    added redux to landing page and account page

--- src/js/components/LandingPage/LandingPage.jsx
+++ src/js/components/LandingPage/LandingPage.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import {
+  getAddress
+} from './landingPageActions';
+import {
+  Link
+} from 'react-router-dom';
 
 export default class LandingPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.updateAddress = this.updateAddress.bind(this);
   }
+
   geolocate() {
-            if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(function(position) {
-                var geolocation = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude
-                };
-                var circle = new google.maps.Circle({
-                  center: geolocation,
-                  radius: position.coords.accuracy
-                });
-                autocomplete.setBounds(circle.getBounds());
-              });
-            }
-        }
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var geolocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          var circle = new google.maps.Circle({
+            center: geolocation,
+            radius: position.coords.accuracy
+          });
+          autocomplete.setBounds(circle.getBounds());
+        });
+      }
+  }
+
+  updateAddress(event){
+    const { dispatch } = this.props;
+    const value = event.target.value;
+    dispatch(getAddress(value));
+  }
         
   render() {
     return (
@@ -30,9 +45,10 @@ export default class LandingPage extends React.Component {
         </div>
         <main>
           <div id="inputGroup">
-            <input placeholder="Enter Location..." type="text" id="locationSearch" onFocus={this.geolocate} />
+            <input placeholder="Enter Location..." type="text" id="locationSearch" onBlur={ this.updateAddress }  />
             <span id="go">Go</span>
           </div>
+          <p id='goToRestaurantPage'>Are you a restaurant owner?<Link style={{'textDecoration': 'none'}} to='/restaurant' ><span style={{'color': 'skyblue'}}> Click here!</span></Link></p>
         </main>
         
       </div>
