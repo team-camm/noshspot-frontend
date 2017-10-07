58de8b053f2508e12395439d4dc0fea18c77ed6e
Cameron Pelina <cpelina88@hotmail.com>
2017-10-06 17:22:59 -0700

    added user registration functionality

--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import landingPageReducer from './components/LandingPage/landingPageReducer';
 import accountInfoReducer from './components/AccountInfo/accountInforeducer';
+import userRegistrationReducer from './components/Registration/userRegistrationReducer';
 
 const rootReducer = combineReducers({
     landingPage: landingPageReducer,
-    accountInfo: accountInfoReducer
+    accountInfo: accountInfoReducer,
+    registeredInfo: userRegistrationReducer
 })
 
 export default rootReducer;
