2fd8833726b8306233078aa6f7b58f4df1c26d35
Alex Black <alexblack426@gmail.com>
2017-10-09 11:01:59 -0700

    added search page and more redux. Also routes

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -8,8 +8,9 @@ import Confirmation from './components/Confirmation/Confirmation';
 import AccountInfo from './components/AccountInfo/index'
 import UserRegistration from './components/Registration/index';
 import RestaurantLanding from './components/RestaurantLanding/RestaurantLanding';
-import CustomerMenu from './components/CustomerMenu/CustomerMenu';
+import CustomerMenu from './components/CustomerMenu/index';
 import RestaurantRegistration from './components/RestaurantRegistration/index';
+import SearchResults from './components/SearchResults/SearchResults';
 
 
 export default class App extends React.Component {
@@ -28,6 +29,7 @@ export default class App extends React.Component {
             <Route path='/restaurant' component={ RestaurantLanding }/>
             <Route path='/restaurantmenu' component={CustomerMenu} /> 
             <Route path='/restaurant-registration' component={ RestaurantRegistration }/>
+            <Route path='/search' component={ SearchResults } />
 
           </div>
         </Router>
--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -3,14 +3,15 @@ import landingPageReducer from './components/LandingPage/landingPageReducer';
 import accountInfoReducer from './components/AccountInfo/accountInforeducer';
 import userRegistrationReducer from './components/Registration/userRegistrationReducer';
 import newRestaurantReducer from './components/RestaurantRegistration/RestaurantRegistrationReducer';
+import customerMenuReducer from './components/CustomerMenu/customerMenuReducer';
 
 
 const rootReducer = combineReducers({
     landingPage: landingPageReducer,
     accountInfo: accountInfoReducer,
     registeredInfo: userRegistrationReducer,
-    newRestaurant: newRestaurantReducer
-
+    newRestaurant: newRestaurantReducer,
+    menu: customerMenuReducer
 })
 
 export default rootReducer;
