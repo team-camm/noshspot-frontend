c138a0fa259fd810707ef02520885b41f201b10c
Alex Black <alexblack426@gmail.com>
2017-11-14 15:17:56 -0800

    capitalized R

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
